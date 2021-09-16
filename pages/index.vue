fb56b98f89a7734ab97563a46f629fbefd88c17d
Luís Caputo <51135242+luiscaputo@users.noreply.github.com>
2021-09-16 17:00:39 +0100

    :cookie: up all

--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,39 @@
     <div>
       <div class="menu-geral">
         <Navigation>
-        <ul class="menu">
-          <li class="item"><NuxtLink class="menu-item" to="/">HOME</NuxtLink></li>
-          <li class="item"><NuxtLink class="menu-item" to="/sobreNos">SOBRE NÓS</NuxtLink></li>
-          <li class="item"><NuxtLink class="menu-item" to="/faleConnosco">FALE CONNOSCO</NuxtLink></li>
-          <li class="item"><NuxtLink class="menu-item" to="/contactos">CONTACTOS</NuxtLink></li>
-        </ul>
-      </Navigation>
+          <ul class="menu">
+            <li class="item">
+              <NuxtLink class="menu-item" to="/">
+                HOME
+              </NuxtLink>
+            </li>
+            <li class="item">
+              <NuxtLink class="menu-item" to="/sobreNos">
+                SOBRE NÓS
+              </NuxtLink>
+            </li>
+            <li class="item">
+              <NuxtLink class="menu-item" to="/faleConnosco">
+                FALE CONNOSCO
+              </NuxtLink>
+            </li>
+            <li class="item">
+              <NuxtLink class="menu-item" to="/contactos">
+                CONTACTOS
+              </NuxtLink>
+            </li>
+          </ul>
+        </Navigation>
       </div>
-      <h1>Hello Nuxters! 👋</h1>
-      <p>
-        This page is rendered on the <strong>{{ rendering }}</strong>
-      </p>
-      <p v-if="rendering === 'server'">
-        First load or hard refresh is done on server side.
-      </p>
-      <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-      <ul>
-        <li>Refresh the page for server side rendering.</li>
-        <li>Click the links to see client side rendering.</li>
-      </ul>
+      <main class="principal-area">
+        <img class="principal-image" src="~/assets/banner.jpg" />
+        <h1 class="text-up">
+          WELCOME TO THE GAMMER WORLD
 
-      <NuxtLink to="/my-profile">About Page</NuxtLink>
-  </div>
+          <p>Hello Brabos aqui você vai encontrar tudo sobre o mundo gammer!</p>
+        </h1>
+      </main>
+    </div>
 </template>
 
 <style>
@@ -33,7 +43,8 @@
     display: inline-block;
     padding: 8px;
     color: grey;
-    margin-right: 20px;
+    margin-right: 0px;
+    float: right;
   }
   .menu-geral{
     width: 960px;
@@ -45,10 +56,31 @@
   .menu-item{
     text-decoration: none;
     color: black;
-    padding: 8px;
+    padding: 16px;
   }
-  .menu{
-    padding: 8;
+  .menu-item:hover
+  {
+    background-color: #BB07E6;
+    border-radius: 8px;
+    color: white;
+    transition: 1s;
+  }
+  .principal-image{
+    margin-left: auto;
+    width: 100%;
+    height: 5%;
+  }
+  .principal-area{
+    margin-top: 3%;
+    font-family: sans-serif;
+  }
+  .text-up{
+    text-align: center;
+    margin-top: -45%;
+    color: white;
+  }
+  img {
+    margin-bottom: 1rem;
   }
 </style>
 
